a3de4b5df4cd919f177fe4adc31f57ab23aa4c9e
Conor <green456@purdue.edu>
2026-01-16 18:17:14 -0500

    current refactor. New NRL and VC versions for sparse formatting

--- make_negishi.sh
+++ make_negishi.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# TODO as CLA
+build_type="Release"
+# build_type="Debug"
+if [ "$1" == "debug" ]; then
+    build_type="Debug"
+fi
+if [ "$1" == "release" ]; then
+    build_type="Release"
+fi
+
+if [ -z "$build_type" ]; then
+    echo "Error: build_type is not set"
+    exit 1
+fi
+
 # in CMakePresets.json : change to "generator": "Unix Makefiles",
 # only needs to be done once
 # sed -i 's/"generator"[[:space:]]*:[[:space:]]*"Ninja"/"generator": "Unix Makefiles"/' CMakePresets.json
@@ -11,9 +26,7 @@ ml boost/1.80.0
 
 echo "Modules loaded"
 
-# TODO as CLA
-build_type="Release"
-#build_type="Debug"
+
 
 cmake --preset ${build_type}
 
